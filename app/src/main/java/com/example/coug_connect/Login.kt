f13a15b363aaa27c337c7f779a14f1fd86408399
Lalith <l.krishnashastry@wsu.edu>
2024-02-17 18:27:01 -0800

    register process 1.1

---
+++ app/src/main/java/com/example/coug_connect/Login.kt
@@ -0,0 +1,11 @@
+package com.example.coug_connect
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Login : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
