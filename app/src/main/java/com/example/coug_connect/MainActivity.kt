c9d01623d15f2898aa91d115bc4e8b09a16e5118
Phearak Both Bunna <phearakboth.bunna@wsu.edu>
2024-02-17 22:40:39 -0800

    Revert "Merge branch 'main' into HomePage"
    This reverts commit 6cc3b1e74a1f45ff332ee9347bf510f5e38c2091, reversing
    changes made to cd2166255b1ab6936de56a4284ce156202ea95f3.

--- app/src/main/java/com/example/coug_connect/MainActivity.kt
+++ app/src/main/java/com/example/coug_connect/MainActivity.kt
@@ -14,8 +14,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.coug_connect.ui.theme.CougConnectTheme
 
-import com.google.firebase.FirebaseApp
-
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -49,47 +47,4 @@ class MainActivity : ComponentActivity() {
 //    CougConnectTheme {
 //        Greeting("Android")
 //    }
-
 //}
-
-        FirebaseApp.initializeApp(this)
-        setContent {
-            CougConnectTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
-                    // Write a message to the database
-//                    val database = Firebase.database
-//                    val myRef = database.getReference("message")
-                    UsersDatabase.writeDataToDatabase()
-                    SubscriptionsDatabase.writeOrganizerDataToDatabase()
-                    SubscriptionsDatabase.writeEventsDataToDatabase()
-                    //myRef.setValue("Hello, World!")
-                }
-            }
-        }
-    }
-
-
-
-
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-            text = "Hello $name!",
-            modifier = modifier
-    )
-
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    CougConnectTheme {
-        Greeting("Android")
-    }
-}
-
-
