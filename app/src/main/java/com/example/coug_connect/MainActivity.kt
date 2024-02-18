2131c0a3ceb20d5b9d13219169b96b6c55aa3e97
Phearak Both Bunna <phearakboth.bunna@wsu.edu>
2024-02-17 22:49:05 -0800

    Revert "Merge pull request #4 from Phearakbothbunna/HomePage"
    This reverts commit f66c0c2a8408e835b80bca1459fb549d25b405ba, reversing
    changes made to 2a538b3ab18caba719fef4057552b310949ae573.

--- app/src/main/java/com/example/coug_connect/MainActivity.kt
+++ app/src/main/java/com/example/coug_connect/MainActivity.kt
@@ -1,8 +1,6 @@
 package com.example.coug_connect
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -17,42 +15,9 @@ import com.google.firebase.FirebaseApp
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 
-
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        setContentView(R.layout.activity_login)
-    }
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//        setContent {
-//            CougConnectTheme {
-//                // A surface container using the 'background' color from the theme
-//                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-//                    Greeting("Android")
-//                }
-//            }
-//        }
-//    }
-}
-
-//@Composable
-//fun Greeting(name: String, modifier: Modifier = Modifier) {
-//    Text(
-//            text = "Hello $name!",
-//            modifier = modifier
-//    )
-//}
-//
-//@Preview(showBackground = true)
-//@Composable
-//fun GreetingPreview() {
-//    CougConnectTheme {
-//        Greeting("Android")
-//    }
-//}
-
         FirebaseApp.initializeApp(this)
         setContent {
             CougConnectTheme {
@@ -87,4 +52,3 @@ fun GreetingPreview() {
         Greeting("Android")
     }
 }
-
