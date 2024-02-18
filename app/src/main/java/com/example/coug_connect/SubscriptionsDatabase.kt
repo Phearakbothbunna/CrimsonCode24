f2c65fac447ec7b8b56f3d4cc81228ab237c2e5f
Phearak Both Bunna <phearakboth.bunna@wsu.edu>
2024-02-18 00:33:44 -0800

    Added db files

---
+++ app/src/main/java/com/example/coug_connect/SubscriptionsDatabase.kt
@@ -0,0 +1,56 @@
+package com.example.coug_connect
+
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
+
+object SubscriptionsDatabase {
+
+    // List of all organizers
+    data class Organizer(val Name: String = "",
+                             val Tag: String = "",
+                             val Subscribers: Map<String, Boolean> = emptyMap(),
+                             val Events: Map<String, Boolean> = emptyMap())
+
+
+
+    data class Events(val EventName: String = "",
+                      val DateTime: Int = 0,
+                      val Description: String = "",
+                      val RSVP: Boolean = false) // YES / NO
+
+
+    fun writeOrganizerDataToDatabase() {
+        val database = Firebase.database
+        val myRef = database.getReference("Subscriptions") // Assume "Subscriptions" as the main node
+
+        // Example of writing data to the database
+        val organizerName = "Organizer" // Unique organizer ID
+        val variab = Organizer(
+            Name = "Wazzu Racing",
+            Tag = "racing",
+            Subscribers = mapOf("subscriber1" to true, "subscriber2" to false), // Example subscribers
+            Events = mapOf("event1" to true, "event2" to true) // Example events
+        )
+
+        myRef.child(organizerName).setValue(variab)
+    }
+
+    fun writeEventsDataToDatabase() {
+        val database = Firebase.database
+        val myRef = database.getReference("Subscriptions/Events") // Assuming "Events" as the main node for events
+
+        // Example of writing data to the database
+        val eventName = "Event1" // Unique event ID
+        val variab = Events(
+            EventName = "Example Event",
+            DateTime = 1234567890, // Example timestamp
+            Description = "This is an example event description.",
+            RSVP = false // Example RSVP status
+        )
+
+        myRef.child(eventName).setValue(variab)
+    }
+
+
+
+}
