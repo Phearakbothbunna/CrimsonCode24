603a40975e72e0458f29f7aa8d1db4957596d4e2
Lalith <l.krishnashastry@wsu.edu>
2024-02-18 02:29:26 -0800

    login and registration added

---
+++ app/src/main/java/com/example/coug_connect/registration.kt
@@ -0,0 +1,12 @@
+package com.example.coug_connect
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+
+class registration : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registration)
+    }
+}
